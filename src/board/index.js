5f71ea50c761b364573d62d5e5a10f2ad93679e2
P2586846 <mahdiharoun1993@gmail.com>
2020-10-30 14:27:39 +0000

    board issues

--- src/board/index.js
+++ src/board/index.js
@@ -4,19 +4,58 @@ import Square from "../square";
 
 function Board() {
   const [grid, setgrid] = useState(Array(9).fill(null));
+  const [isNext,setisNext] = useState(true)
   console.log(grid);
 
   function handleClick(index) {
-    setgrid([...grid.slice(0, index), "X", ...grid.slice(index + 1)]);
+    setgrid([...grid.slice(0, index), isNext ? 'X' : 'O', ...grid.slice(index + 1)]);
+    setisNext(!isNext )
+    if (calculateWinner(grid) || grid[index] ){
+      return
+    } 
   }
 
+  function calculateWinner(grid) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (grid[a] && grid[a] === grid[b] && grid[a] === grid[c]) {
+        console.log("working")
+        return grid[a];
+      }
+    }
+    return null;
+  }  
+
+   function render() {
+    const winner = calculateWinner(grid);
+    
+    if (winner) {
+       return  'Winner: ' + winner;
+    } else {
+      return 'Next player: ' + (isNext ? 'X' : 'O');
+    }
+  
   return (
+     
     <div>
+      <h1>  Next player: {isNext ? 'X' : 'O'} </h1>
+      <h2>{render()}</h2>
       {grid.map((grid, index) => (
         <Square index={index} onHandle={handleClick} text={grid} />
       ))}
     </div>
   );
+}
 }
 export default Board;
 
